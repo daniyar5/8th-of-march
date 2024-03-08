37e79d6176a753aa25a798845d8fa28b1e66a897
kimashiidan <68999088+daniyar5@users.noreply.github.com>
2024-03-08 10:13:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-    $('.me').mouseenter(function () {
-        $('.me').stop().animate({
-            scale: 1.05
-        });
-    }).mouseleave(function () {
-        $('.me').stop().animate({
-            scale: 1
-        });
-    });
+// $(document).ready(function () {
+//     $('.me').mouseenter(function () {
+//         $('.me').stop().animate({
+//             scale: 1.05
+//         });
+//     }).mouseleave(function () {
+//         $('.me').stop().animate({
+//             scale: 1
+//         });
+//     });
 
 
     $('.button-33').on("click", function(){
