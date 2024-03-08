d3d123272746870a9735c8e6053c115b6a2c553f
kimashiidan <68999088+daniyar5@users.noreply.github.com>
2024-03-08 10:08:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.me').mouseenter(function () {
+        $('.me').stop().animate({
+            scale: 1.05
+        });
+    }).mouseleave(function () {
+        $('.me').stop().animate({
+            scale: 1
+        });
+    });
+
+
+    $('.button-33').on("click", function(){
+        $('.flowers img').fadeToggle("slow");
+    })
+});
+
+
